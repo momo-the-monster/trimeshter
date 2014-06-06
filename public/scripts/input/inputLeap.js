afb74e78a5c27aac17782c01327be1531e05e52c
Momo the Monster <surya@mmmlabs.com>
2014-06-05 17:16:44 -0700

    wrap leapInput in new mmmInput namespace

--- public/scripts/input/inputLeap.js
+++ public/scripts/input/inputLeap.js
@@ -1,73 +1,90 @@
-// Store ongoing touches to detect enter & leave events
-var leapTouches = new Array();
+var mmmInput = mmmInput || {};
 
-/**
- * Initialize Leap Controller and rigged hand
- */
-(window.controller = new Leap.Controller)
-    .use('riggedHand', {
-        parent: scene,
-        renderer: renderer,
-        renderFn: function () {
-        },
-        scale: 2,
-        offset: new THREE.Vector3(0, -30, 10),
-        camera: camera,
-        positionScale: 2
-    })
-    .connect();
-// rigged hand styling
-controller.on('riggedHand.meshAdded', function (handMesh, leapHand) {
-    handMesh.material.opacity = 0.9;
-});
+mmmInput.LeapInput = mmmInput.LeapInput || {};
 
-/**
- * Leap Frame Loop
- * Find all fingers on all hands
- * Test each one for its Touch Zone - https://developer.leapmotion.com/documentation/javascript/devguide/Leap_Touch_Emulation.html
- * Call onStart, onMove and onFinish which are root-level events for the scene
- */
-controller.on('frame', function (frame) {
+var LeapInput = mmmInput.LeapInput = function LeapInput(options) {
+    options = options || {};
+    this.onStart = options.onStart || null;
+    this.onMove = options.onMove || null;
+    this.onEnd = options.onEnd || null;
+    this.leapTouches = [];
+    var self = this;
 
-    // loop through every hand
-    for (var h = 0; h < frame.hands.length; h++) {
+    /**
+     * Initialize Leap Controller and rigged hand
+     */
+    (window.controller = new Leap.Controller)
+        .use('riggedHand', {
+            parent: scene,
+            renderer: renderer,
+            renderFn: function () {
+            },
+            scale: 2,
+            offset: new THREE.Vector3(0, -30, 10),
+            camera: camera,
+            positionScale: 2
+        })
+        .connect();
+    // rigged hand styling
+    controller.on('riggedHand.meshAdded', function (handMesh, leapHand) {
+        handMesh.material.opacity = 0.9;
+    });
 
-        var hand = frame.hands[h];
-        var handMesh = frame.hands[h].data('riggedHand.mesh');
+    /**
+     * Leap Frame Loop
+     * Find all fingers on all hands
+     * Test each one for its Touch Zone - https://developer.leapmotion.com/documentation/javascript/devguide/Leap_Touch_Emulation.html
+     * Call onStart, onMove and onFinish which are root-level events for the scene
+     * These are passed in via the constructor above
+     */
+    controller.on('frame', function (frame) {
 
-        // loop through every finger
-        for (var i = 0; i < hand.fingers.length; i++) {
+        // loop through every hand
+        for (var h = 0; h < frame.hands.length; h++) {
 
-            var finger = hand.fingers[i];
-            var id = finger.id;
+            var hand = frame.hands[h];
+            var handMesh = frame.hands[h].data('riggedHand.mesh');
 
-            // find existing leap touch to detect Start, Move and End
-            // ongoing touch logic based on https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Touch_events
-            var idx = isLeapPressed(id);
+            // loop through every finger
+            for (var i = 0; i < hand.fingers.length; i++) {
 
-            // get scene position
-            var scenePosition = handMesh.screenPosition(hand.fingers[i].stabilizedTipPosition, camera);
-            var event = {x: scenePosition.x, y: window.innerHeight - scenePosition.y, id: id};
+                var finger = hand.fingers[i];
+                var id = finger.id;
 
-            if (finger.touchZone == "touching") {
-                if (idx >= 0) {
-                    onMove(event);
-                    leapTouches.splice(idx, 1, event);
+                // find existing leap touch to detect Start, Move and End
+                // ongoing touch logic based on https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Touch_events
+                var idx = self.isLeapPressed(id);
+
+                // get scene position
+                var scenePosition = handMesh.screenPosition(hand.fingers[i].stabilizedTipPosition, camera);
+                var event = {x: scenePosition.x, y: window.innerHeight - scenePosition.y, z:scenePosition.z, id: id};
+
+                if (finger.touchZone == "touching") {
+                    if (idx >= 0) {
+                        if(self.onMove !== null) {
+                            self.onMove(event);
+                        }
+                        self.leapTouches.splice(idx, 1, event);
+                    } else {
+                        if(self.onStart !== null) {
+                            self.onStart(event);
+                        }
+                        self.leapTouches.push(event);
+                    }
                 } else {
-                    onStart(event);
-                    leapTouches.push(event);
+                    // test that a touch exists before we try to finish it
+                    if (idx >= 0) {
+                        if(self.onEnd !== null) {
+                            self.onEnd(event);
+                        }
+                        self.leapTouches.splice(idx, 1);
+                    }
                 }
-            } else {
-                // test that a touch exists before we try to finish it
-                if (idx >= 0) {
-                    onFinish(event);
-                    leapTouches.splice(idx, 1);
-                }
-            }
 
-        } // end of finger loop
-    } // end of hand loop
-});
+            } // end of finger loop
+        } // end of hand loop
+    });
+};
 
 /**
  * Track whether a touch has already started
@@ -75,11 +92,11 @@ controller.on('frame', function (frame) {
  * @param idToFind
  * @returns {number}
  */
-function isLeapPressed(idToFind) {
-    for (var i = 0; i < leapTouches.length; i++) {
-        if (leapTouches[i].id == idToFind) {
+LeapInput.prototype.isLeapPressed = function(idToFind) {
+    for (var i = 0; i < this.leapTouches.length; i++) {
+        if (this.leapTouches[i].id == idToFind) {
             return i;
         }
     }
     return -1;    // not found
-}
+};
