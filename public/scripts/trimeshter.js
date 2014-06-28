30726f39b675061069ed6ae0650afa9fd8f2679d
Momo the Monster <surya@mmmlabs.com>
2014-06-28 16:22:32 -0700

    trimeshter - incoming cursors no longer need normalization

--- public/scripts/trimeshter.js
+++ public/scripts/trimeshter.js
@@ -391,11 +391,9 @@ var Trimeshter = mmm.Trimeshter = function Trimeshter(canvas) {
      * @param event has x, y, id
      */
     function onMove(event) {
-     //   console.log('tri:', event.x, event.y);
-        var x = ( event.x / window.innerWidth ) * 2 - 1;
-        var y = -( event.y / window.innerHeight ) * 2 + 1;
+        var x = event.x * 2 - 1;
+        var y = - event.y * 2 + 1;
         var z = event.z || 0;
-    //    console.log('tri:', x, y);
 
         var position = getWorldPosition(x, y);
         if (position) {
@@ -499,8 +497,8 @@ var Trimeshter = mmm.Trimeshter = function Trimeshter(canvas) {
      */
     function onEnd(event) {
 
-        var x = ( event.x / window.innerWidth ) * 2 - 1;
-        var y = -( event.y / window.innerHeight ) * 2 + 1;
+        var x = event.x * 2 - 1;
+        var y = -event.y * 2 + 1;
         var z = event.z || getRandomArbitrary(config.randomZ, -config.randomZ);
         var self = this;    // store this for use in Tween functions below
 
