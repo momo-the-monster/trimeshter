3ed3f0982914cce50a63b68506d058bf96511d9f
Momo the Monster <surya@mmmlabs.com>
2014-07-28 07:45:48 -0700

    send FiddlyBits as default touch interface

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.static('public'));
 
 // Send the client html.
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/public/interfaces/touch.html')
+    res.sendfile(__dirname + '/public/interfaces/fiddlybits.html')
 });
 
 app.listen(3456, function () {
